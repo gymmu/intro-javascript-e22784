8a2c33fca6af923f1258e03045fbbd6da904fb90
Jaël Theiler <e227484@edu.sbl.ch>
2023-10-16 12:12:31 +0200

    Ende Lektion 16/10/23

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 function sayHello() {
   alert("Hello World!")
 }
@@ -5,4 +7,18 @@ function sayHello() {
 function changeBackground() {
   const body = document.querySelector("body")
   body.style.backgroundColor = "darkgrey"
+}
+
+
+function deleteText() {
+  const elemToDeleteText = document.querySelector("#to-delete")
+  elemToDeleteText.textContent = "Inhalt gelöscht"
+}
+
+
+function plusThree() {
+  const numberElement = document.querySelector("#zahl")
+  let num = parseInt(numberElement.textContent)
+  num = num + 3
+  numberElement.textContent = num
 }
